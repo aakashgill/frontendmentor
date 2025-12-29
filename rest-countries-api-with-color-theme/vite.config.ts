8b2c268891513f680f21a9f534b421f5e4e45581
Aakash Gill <aakashgill210@gmail.com>
2025-12-29 20:29:57 +0530

    move react build to root to see if it works

--- rest-countries-api-with-color-theme/vite.config.ts
+++ rest-countries-api-with-color-theme/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        app: './main.html', // Specify main.html as the entry point for the build
+      },
+    },
+  },
+  server: {
+    open: '/main.html', // Automatically open main.html when you run the dev server
+  }
 })
